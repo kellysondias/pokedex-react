f9924643cb9cfc84029d239e5af07d472539ecd9
Kellyson <Kellysondias@outlook.com>
2022-07-04 18:03:52 -0300

    Rendering pokemon data

--- src/App.jsx
+++ src/App.jsx
@@ -15,18 +15,31 @@ function App() {
     }
     fetchData()
   }, [])
-  console.log(pokedex)
+  /* console.log(pokedex) */
   return (
     <>
       <GlobalStyle />
       <Header />
       <ul>
-        {pokedex.map((pokemon, index) => 
-                                <li key={index}>
-                                  <div>
-                                    <img src={pokemon.sprites.front_default} alt={`${pokemon.name}'s appearance`}></img>
-                                  </div>
-                                </li>)}
+        {pokedex.map((pokemon, index) => {
+          const typeNames = pokemon.types.map(type => type.type.name)
+          const [type1, type2] = typeNames
+          let pokemonNumber = pokemon.id < 10 ? <span>{`#00${pokemon.id}`}</span> : <span>{`#0${pokemon.id}`}</span>
+          
+          return <li key={index}>
+                  <div>
+                    <div>
+                      <img src={pokemon.sprites.front_default} alt={`${pokemon.name}'s appearance`} />
+                      <span>{pokemon.name}</span>
+                      {pokemon.id < 100 ? <span>{pokemonNumber}</span> : <span>{`#${pokemon.id}`}</span> }
+                    </div>
+                    <div>
+                      <span>{type1}</span>
+                      {type2 && <span>{type2}</span>}
+                    </div>
+                  </div>
+                </li>
+        })}
       </ul>
     </>
   );
