c933b851292028085dd1d27a5c8358e78f738390
Kellyson <Kellysondias@outlook.com>
2022-08-11 17:52:57 -0300

    Configuring the 'pokemon' page

--- src/services/endpoints.js
+++ src/services/endpoints.js
@@ -1,5 +1,5 @@
 const baseUrl = 'https://pokeapi.co/api/v2/pokemon'
-const offSetValue = 257
+const offSetValue = 0
 
 export async function getPokemon(name, id) {
     const url = await fetch(`${baseUrl}/${name || id}`)
